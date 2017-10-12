39a64e3270baf4dbfb8224f5c57bbf264f7c3504
Andrei Balan <andrei.balan@ownzones.com>
2017-10-12 13:04:28 +0300

    Jest Fixes

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -4,12 +4,8 @@ import { shallow } from 'enzyme';
 import Button from './Button';
 
 describe('Button', () => {
-
   it('render a button', () => {
-    const wrapper = shallow(<Button loading />);
-    // expect(wrapper).toMatchSnapshot();
-
+    shallow(<Button loading />);
   });
-
 });
 
